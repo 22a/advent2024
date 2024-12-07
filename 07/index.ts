106f49734c8936ab86bacf197427f498d0e3b476
Peter Meehan <peter.meehan@intercom.io>
2024-12-07 14:01:12 +0000

    day 07: clarified variable names :+1:

--- 07/index.ts
+++ 07/index.ts
@@ -1,9 +1,9 @@
-import { readInput as a } from '../utils/readInput.ts';
-const s = a(import.meta.url, process.argv.includes('--example'));
-const n = s.split('\n').map((r) => r.split(': ').flatMap((c) => c.split(' ').map(Number)));
+import { readInput as s } from '../utils/readInput.ts';
+const t = s(import.meta.url, process.argv.includes('--example'));
+const r = t.split('\n').map((r) => r.split(': ').flatMap((r) => r.split(' ').map(Number)));
 const c = (r: number, t: number, [s, ...n]: number[], e: string[]): boolean => {
   if (s === undefined) return t === r;
   return e.map((p) => Number(p === '|' ? `${t || ''}${s}` : eval(`${t}${p}${s}`))).some((t) => c(r, t, n, e));
 };
-console.log(`Part 1: ${n.filter(([r, ...t]) => c(r, 0, t, ['+', '*'])).reduce((r, t) => r + t[0], 0)}`);
-console.log(`Part 2: ${n.filter(([r, ...t]) => c(r, 0, t, ['+', '*', '|'])).reduce((r, t) => r + t[0], 0)}`);
+console.log(`Part 1: ${r.filter(([r, ...t]) => c(r, 0, t, ['+', '*'])).reduce((r, t) => r + t[0], 0)}`);
+console.log(`Part 2: ${r.filter(([r, ...t]) => c(r, 0, t, ['+', '*', '|'])).reduce((r, t) => r + t[0], 0)}`);
