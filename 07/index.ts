79610a80bf0399f96a4cbdb0f4ab2755f16de0ae
Peter Meehan <peter.meehan@intercom.io>
2024-12-07 12:39:33 +0000

    more manual disgusting mangling

--- 07/index.ts
+++ 07/index.ts
@@ -1,20 +1,16 @@
-import { readInput } from '../utils/readInput.ts';
-const s = readInput(import.meta.url, process.argv.includes('--example'));
+import { readInput as a } from '../utils/readInput.ts';
+const s = a(import.meta.url, process.argv.includes('--example'));
 const n = s.split('\n').map((r) => {
   const [t, o] = r.split(': ');
   const e = o.split(' ').map(Number);
   return { i: Number(t), t: e };
 });
-const m = { o: (r, t) => r + t, m: (r, t) => r * t, p: (r, t) => Number(`${r}${t}`) };
-const c = (r, t, o, e) => {
+type x = number;
+const m = { o: (r: x, t: x) => r + t, m: (r: x, t: x) => r * t, p: (r: x, t: x) => Number(`${r}${t}`) };
+const c = (r: x, t: x, o: x[], e = [m['o'], m['m'], m['p']]) => {
   if (o.length === 0) return t === r;
   const [s, ...n] = o;
-  const m = e.map((r) => r(t, s));
-  return m.some((t) => c(r, t, n, e));
+  return e.map((r) => r(t, s)).some((t) => c(r, t, n, e));
 };
-let p = n.filter(({ i: r, t: t }) => c(r, 0, t, [m['o'], m['m']]));
-let u = p.reduce((r, t) => r + t.i, 0);
-console.log('Part 1:', u);
-p = n.filter(({ i: r, t: t }) => c(r, 0, t, [m['o'], m['m'], m['p']]));
-u = p.reduce((r, t) => r + t.i, 0);
-console.log('Part 2:', u);
+console.log(`Part 1: ${n.filter(({ i: r, t: t }) => c(r, 0, t, [m['o'], m['m']])).reduce((r, t) => r + t.i, 0)}`);
+console.log(`Part 2: ${n.filter(({ i: r, t: t }) => c(r, 0, t)).reduce((r, t) => r + t.i, 0)}`);
